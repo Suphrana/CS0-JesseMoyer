c4b7a4af76fe231bf81e5efcd57e27e408eb7267
Jesse Moyer <jjmoyer31@gmail.com>
2022-02-02 16:16:07 -0700

    Adding comments in different formats.

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+'''
+Name: Jesse Moyer
+Date: 02/02/22
+
+This program prints out Hello World for our first program.
+'''
+
+# This is a comment
+# This is a second line
+print("Hello World!") #This is another comment
