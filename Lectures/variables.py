4ef1d3ae3225171d7a1c19423216999a7724ff3b
Jesse Moyer <jjmoyer31@gmail.com>
2022-02-07 16:49:49 -0700

    Finding area of rectangle with user inputs

---
+++ Lectures/variables.py
@@ -0,0 +1,15 @@
+'''
+Name: Jesse Moyer
+Date: 2/7/2022
+
+Finding area and perimeter of a rectangle with user inputs.
+
+'''
+rectLength=input('What is the length of the rectangle? ->')
+rectWidth=input('What is the width of the rectangle? ->' )
+rectLength = int(rectLength)
+rectWidth=int(rectWidth)
+area=(rectLength*rectWidth)
+print('The area of the rectangle is:', area)
+perimeter=(2*(rectWidth+rectLength))
+print('The perimeter of the rectangle is:', perimeter)
