89774fb444416ec861c488730a3e3d2237a39376
Jesse Moyer <jjmoyer31@gmail.com>
2022-02-11 16:47:37 -0700

    Started Hangman project and reached stage 0

---
+++ Stdio/stdio.py
@@ -0,0 +1,26 @@
+'''
+Name: Jesse Moyer
+Date: 2/11/2022
+
+Ascii hangman assignment - prints various stages of the hangman game
+using ascii art.
+
+'''
+#Greeting and username input
+userName=input("Hey pal, what's your name? ->")
+
+print("Hello,", userName)
+
+#Stage 0
+print("    |__________") 						
+print("    |/     |") 
+print("    |") 	
+print("    |")   
+print("    |") 	
+print("    |")
+print("    |") 				              
+print("-------------") 		
+
+#Stage 1
+
+
