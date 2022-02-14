ec217df5ada9e0d2d254b3d38170dc933bc99a9f
Jesse Moyer <jjmoyer31@gmail.com>
2022-02-14 16:49:46 -0700

    Completed hangman project and added printed stages

---
+++ Assignments/stdio.py
@@ -0,0 +1,88 @@
+'''
+Name: Jesse Moyer
+Date: 2/11/2022
+
+Ascii hangman assignment - prints various stages of the hangman game
+using ascii art.
+
+'''
+#Greeting and username input
+userName=input("Hey pal, what's your name? ->")
+
+print("Hello,", userName)
+
+print("Stage 1")
+print("    |__________") 						
+print("    |/     |") 
+print("    |") 	
+print("    |")   
+print("    |") 	
+print("    |")
+print("    |") 				              
+print("-------------")
+print("\n") 		
+
+print("Stage 2")
+print("    |__________") 						
+print("    |/     |") 
+print("    |      O") 	
+print("    |")   
+print("    |") 	
+print("    |")
+print("    |") 				              
+print("-------------")
+print("\n") 		
+
+print("Stage 3")
+print("    |__________") 						
+print("    |/     |") 
+print("    |      O") 	
+print("    |      |")   
+print("    |") 	
+print("    |")
+print("    |") 				              
+print("-------------")
+print("\n") 		
+
+print("Stage 4")
+print("    |__________") 						
+print("    |/     |") 
+print("    |      O") 	
+print("    |     /|")   
+print("    |") 	
+print("    |")
+print("    |") 				              
+print("-------------")
+print("\n") 	
+
+print("Stage 5")
+print("    |__________") 						
+print("    |/     |") 
+print("    |      O") 	
+print("    |     /|\ ")   
+print("    |") 	
+print("    |")
+print("    |") 				              
+print("-------------")
+print("\n") 	
+
+print("Stage 6")
+print("    |__________") 						
+print("    |/     |") 
+print("    |      O") 	
+print("    |     /|\ ")   
+print("    |     /") 	
+print("    |")
+print("    |") 				              
+print("-------------")
+print("\n") 	
+
+print("Stage 7")
+print("    |__________") 						
+print("    |/     |") 
+print("    |      O") 	
+print("    |     /|\ ")   
+print("    |     / \ ") 	
+print("    |")
+print("    |") 				              
+print("-------------") 
