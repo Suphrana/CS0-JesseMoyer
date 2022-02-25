fd72cee6a046f85cebc3cb385f8a2e34a74772b0
Jesse Moyer <98365320+Suphrana@users.noreply.github.com>
2022-02-25 15:47:56 -0700

    Edited triangle program to condense.

--- Triangle/Triangle.py
+++ Triangle/Triangle.py
@@ -4,29 +4,22 @@ Date: 2/9/2022
 
 Finding the area and perimeter of a triangle using 3 sides
 
-'''
-#Importing a math function
-from math import sqrt 
+''' 
 
 #Assigning variables to user inputs
 a=float(input("Side a ->"))
 b=float(input('Side b ->'))
 c=float(input("Side c ->"))
 
+#Created a function with all of the formulas in order to compress
+def demensions():
+    perimeter = (a+b+c)
+    s = perimeter/2
+    area = (s*(s-a)*(s-b)*(s-c))**1/2
+    print(f"These sides form a triangle with Area: {area}, Perimeter: {perimeter}")
+
 #Determines whether the sides form a triangle or not
 if (a+b < c) or (a+c < b) or (b+c < a):
     print("These sides do not form a triangle.")
 else:
-    print("These sides form a triangle.")
-
-#Semiperimeter formula
-s=(((a+b+c)/2))
-
-#Assigning equations to a variable
-Perimeter=float(a+b+c)
-Area= float(sqrt(s*(s-a)*(s-b)*(s-c)))
-
-#Printing answers to show user
-print("Area:", Area)
-print("Perimeter:", Perimeter)
-
+    demensions()
