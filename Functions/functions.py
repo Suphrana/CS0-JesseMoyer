f6861c4773ce101bddd07d97f37709d7ad23c8aa
Suphrana <jjmoyer31@gmail.com>
2022-02-25 16:50:08 -0700

    function and conditionals work

---
+++ Functions/functions.py
@@ -0,0 +1,13 @@
+'''
+Name: Jesse Moyer
+Date: 02/25/22
+
+Write a Python program that performs some arithmetic operations
+on two given numbers (entered by user) along with the following requirements.
+
+'''
+#User input function to gather 2 numbers
+print("Please enter two positive integers:")
+num1, num2 = float(input()), float(input())
+
+
---
+++ Lectures/conditionals.py
@@ -0,0 +1,14 @@
+'''
+Name: Jesse Moyer
+Date: 02/25/22
+'''
+def main():
+    x = 5
+    y= 10
+    print(f"Before conditional")
+    if (x!=y):
+        print("x is NOT equal to y")
+
+    print(f"After conditional")
+
+main()
