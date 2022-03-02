853098f773a89f09614da54dae7fd7cffd7047fe
Suphrana <jjmoyer31@gmail.com>
2022-03-02 16:19:17 -0700

    Functions revised to work properly

--- Functions/functions.py
+++ Functions/functions.py
@@ -6,18 +6,22 @@ Write a Python program that performs some arithmetic operations
 on two given numbers (entered by user) along with the following requirements.
 
 '''
+#Import sqrt function
+from math import sqrt 
+
 #Main function:
 #User inputs for 2 numbers
 def main():
     print("Please enter two positive integers:")
     num1, num2 = float(input()), float(input())
+    test()
     print("Sum",addition(num1, num2))
     print("Product",multiplicaiton(num1, num2))
     print("Quotient",division(num1, num2))
     print("Difference",subtraction(num1, num2))
     print("Remainder",modulus(num1, num2))
     print("Power",power(num1, num2))
-    print("Square root",root(num1, num2))
+    print("Square root of the first number",root(num1))
     print(bigger_number(num1, num2))
     return num1, num2
 
@@ -40,8 +44,8 @@ def modulus(num1, num2):
 def power(num1, num2):
     return num1**num2
 
-def root(num1, num2):
-    return num1**(1/num2)
+def root(num1):
+    return sqrt(num1)
 
 #Extra Credit
 def bigger_number(num1, num2):
@@ -53,27 +57,28 @@ def bigger_number(num1, num2):
         print(f"{num2} is greater than {num1}.")
 
 #Test function
-def test(num1, num2):
+def test():
     assert addition(5,5) == 10
-    assert
+    assert addition(6,9) == 15
 
-    assert
-    assert
+    assert multiplicaiton(2,3) == 6
+    assert multiplicaiton(8,8) == 64
 
-    assert
-    assert
+    assert division(4,2) == 2
+    assert division(8,2) == 4
 
-    assert
-    assert
+    assert subtraction(20,10) == 10
+    assert subtraction(35,5) == 30
 
-    assert
-    assert
+    assert modulus(5,5) == 0
+    assert modulus(4,3) == 1
 
-    assert
-    assert
+    assert power(4,2) == 16
+    assert power(5,2) == 25
 
-    assert
-    assert
+    assert root(4) == 2
+    assert root(16) == 4
+    print("All test cases passed")
     
 
 #Function placed to output everything within
