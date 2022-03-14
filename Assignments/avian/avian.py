dc76ff0f423d62fe9f1cfc9daba6fb5041ea8192
Jesse Moyer <jjmoyer31@gmail.com>
2022-03-14 16:50:01 -0600

    Avian progress

---
+++ Assignments/avian/avian.py
@@ -0,0 +1,15 @@
+'''
+Name: Jesse Moyer
+Date: 03/14/2022
+
+Solve the avian kattis problem using python 3
+'''
+def main():
+    b1="47-FBI"
+    b2="BOND-007"
+    b3="RF-FBI18"
+    b4="MARICA-13"
+    b5="13A-FBILL"
+   
+    
+main()
