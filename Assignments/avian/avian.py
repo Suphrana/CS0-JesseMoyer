f1c9272e20a8589405479b44c0543fb1e4ad1e95
Jesse Moyer <98365320+Suphrana@users.noreply.github.com>
2022-03-15 02:32:24 -0600

    whoops edited fixed

--- Assignments/avian/avian.py
+++ Assignments/avian/avian.py
@@ -11,7 +11,7 @@ def main():
 
     #FBI blimp counter
     for i in range(5):
-        blimp = input("Gay")
+        blimp = input()
         if "FBI" in blimp:
             CIA.append(str(i+1))
             
