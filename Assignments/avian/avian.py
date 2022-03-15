725124c621111aaf2230e015234839d284173f20
Jesse Moyer <98365320+Suphrana@users.noreply.github.com>
2022-03-15 03:21:45 -0600

    Added avian screenshot, need test

--- Assignments/avian/avian.py
+++ Assignments/avian/avian.py
@@ -4,20 +4,22 @@ Date: 03/14/2022
 
 Solve the avian kattis problem using python 3
 '''
+#Main function
 def main():
     #Storage for blimp reg numbers
     CIA = []
-
 
     #FBI blimp counter
     for i in range(5):
         blimp = input()
         if "FBI" in blimp:
             CIA.append(str(i+1))
-            
+    #Add a space between the numbers if CIA has contents        
     if CIA:
         print(" ".join(CIA))
+    #No contents = he escaped   
     else:
         print("HE GOT AWAY!")
 
 main()
+
