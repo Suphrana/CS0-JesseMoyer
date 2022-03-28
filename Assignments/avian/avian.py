35ce82c276809bfa3875b5a97b283bc49e83a1a0
Jesse Moyer <jjmoyer31@gmail.com>
2022-03-28 16:50:50 -0600

    1 test added

--- Assignments/avian/avian.py
+++ Assignments/avian/avian.py
@@ -22,9 +22,37 @@ def main():
     else:
         print("HE GOT AWAY!")
     print(" ".join(CIA))
-    return CIA
+    
+
+    def test():
+    #Storage for blimp reg numbers
+        CIA = []
+
+    #FBI blimp counter
+    
+        blimp = ["FBI","HUG","FBI","BRUH","AHAH"]
+        if "FBI" in blimp[0]:
+                CIA.append(str(+1))
+        if "FBI" in blimp[1]:
+                CIA.append(str(+2))
+        if "FBI" in blimp[2]:
+                CIA.append(str(+3))
+        if "FBI" in blimp[3]:
+                CIA.append(str(+4))
+        if "FBI" in blimp[4]:
+                CIA.append(str(+5))
+        #Add a space between the numbers if CIA has contents        
+        if CIA:
+            " ".join(CIA)
+        #No contents = he escaped   
+        else:
+            print("HE GOT AWAY!")
+        
+        assert CIA == ['1','3']
+    test()
 
 main()
+
 
 
 
