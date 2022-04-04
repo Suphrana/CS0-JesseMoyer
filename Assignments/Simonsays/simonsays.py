b3f09e91bf66c9bd0b25bf26b28d6290c2de2509
Jesse Moyer <jjmoyer31@gmail.com>
2022-04-04 16:24:11 -0600

    Simon says complete, checking kattis

--- Assignments/Simonsays/simonsays.py
+++ Assignments/Simonsays/simonsays.py
@@ -4,26 +4,37 @@ Date: 03/30/2022
 
 Create a computer program that can play simon says perfectly
 '''
-
+#User inputs amount of lines for game
 def input_turns():
-    turn = int(input("Number of simon says commands ->"))
+    turn = int(input())
     return turn
 
+#Grabs words after simon says if string has simon says
 def simonsays(strings):
-        if "Simon says " in strings:
-            print(strings[10:])
-        return strings
+    if "Simon says " in strings:
+        print(strings[10:])
+    return strings
 
+#User inputs the lines for game
 def input_string():
-    inputted_string = input("Simon says commands ->")
+    inputted_string = input()
     return inputted_string
 
-# def test():
-#     assert (simonsays("Simon says die") == " die")
+#Test function, removed print to work for test
+def simonsaystest(strings):
+    if "Simon says " in strings:
+        return strings[10:]
 
+#Main function assigns variables and combines to create game
 def main():
     turns = input_turns()
     for i in range (turns):
         strings = input_string()
-    simonsays(strings)
+        simonsays(strings)
+
+    #test functions   
+    assert (simonsaystest("Simon says bruh") == " bruh")
+    assert (simonsaystest("Simon says jump") == " jump")
+    assert (simonsaystest("Simon says lol") == " lol")
 main()
+
